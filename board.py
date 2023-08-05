eac27d95bb5a5da82876ed88f7975fccba66e926
tatougueye <141055942+tatougueye@users.noreply.github.com>
2023-08-05 07:18:10 -0400

    Add files via upload
    player vs player mode done

--- board.py
+++ board.py
@@ -9,12 +9,32 @@ class Board:
         drawRect(app.boardLeft, app.boardTop, app.boardWidth, app.boardHeight,
              fill = "yellow")
     def drawBoardMessage(self, app):
-        if app.gameOver:
-            drawLabel(app.gameOverMessage, (app.boardLeft+app.boardWidth)/2, 
+        drawLabel(app.gameOverMessage, app.width/2, 
                       app.boardTop - 30, align = "center", fill = "red", bold = True)
+    def checkWinner(self, app):
+        if app.gameOver:
+            if len(app.storeAMarbles) > len(app.storeBMarbles):
+                winner = app.playerAName
+                loser = app.playerBName
+                drawLabel(f"{winner} IS THE WINNER!", app.width/2, 30, fill = "gold")
+                drawLabel(f"{winner} : {len(app.storeAMarbles)} points", app.width/2, 60)
+                drawLabel(f"{loser} : {len(app.storeBMarbles)} points", app.width/2, 80)
 
-            
-    
+            elif len(app.storeBMarbles) > len(app.storeAMarbles):
+                winner = app.playerBName
+                loser = app.playerAName
+                drawLabel(f"{winner} IS THE WINNER!", app.width/2, 30, fill = "gold")
+                drawLabel(f"{winner} : {len(app.storeBMarbles)} points", app.width/2, 60)
+                drawLabel(f"{loser} : {len(app.storeAMarbles)} points", app.width/2, 80)
+
+            else:
+                drawLabel(f"IT'S A TIE!", app.width/2, 30, fill = "gold")
+                drawLabel(f"{app.playerAName} : {len(app.storeAMarbles)} points", app.width/2, 60)
+                drawLabel(f"{app.playerBName} : {len(app.storeBMarbles)} points", app.width/2, 80)
+                
+    # def drawWinnerMessage(self, app):
+    #     playerWinner = 
+    #     if 
     
 class StoreA:
     def __init__(self, app):
@@ -65,11 +85,11 @@ class PitsA:
             for cxMarble, cyMarble in pitAndMarbles[1]:
                 drawCircle(cxMarble, cyMarble, app.radiusMarble, fill = "pink")
     
-    def allEmpty(self, app):
-        for circleCheck in app.marblesA:
-            if circleCheck[1] != []:
-                return False
-        return True 
+    # def allEmpty(self, app):
+    #     for circleCheck in app.marblesA:
+    #         if circleCheck[1] != []:
+    #             return False
+    #     return True 
 
 class PitsB:
     def __init__(self, app):
@@ -88,11 +108,11 @@ class PitsB:
             for cxMarble, cyMarble in pitAndMarbles[1]:
                 drawCircle(cxMarble, cyMarble, app.radiusMarble, fill = "pink")
     
-    def allEmpty(self, app):
-        for circleCheck in app.marblesB:
-            if circleCheck[1] != []:
-                return False
-        return True 
+    # def allEmpty(self, app):
+    #     for circleCheck in app.marblesB:
+    #         if circleCheck[1] != []:
+    #             return False
+    #     return True 
 
 
 
@@ -102,11 +122,10 @@ class PlayerA:
         self.app = app
    
     def moveMarblesInpit(self, app, cxTarget, cyTarget):
-        # app.messageTurnA = f"{app.playerAName}'s Turn"
+        app.messageTurnA = f"{app.playerAName}'s Turn"
         indexTarget = PlayerA.getIndexCircleTarget(app, cxTarget, cyTarget)
         numMarbles = len(app.marblesA[indexTarget][1])
-
-        if numMarbles > 0:
+        if app.marblesA[indexTarget][1] != []:
             nextCircle = indexTarget
             oppCircle = 5
             while numMarbles != 0:
@@ -135,6 +154,7 @@ class PlayerA:
                 #made a turn of the whole board and have to deposit in my board again
                 #case if I have a lot of marbles in chosen pit 
                 else:
+                    # playerA.moveMarblesInpit(app, cxTarget, cyTarget)
                     nextCircle = -1
             app.playerATurn = False
             app.playerBTurn = True 
@@ -163,11 +183,12 @@ class PlayerA:
     def drawMessageTurnA(self, app):
         drawLabel(app.messageTurnA, (app.boardLeft + app.boardWidth - 30), 
                   (app.boardTop + app.boardHeight + 30), fill = "black", align = "center" )
-    
+
     def captureAllRestMarbles(self, app):
         marblesLeftA = 0
         for pit in app.marblesA:
             marblesLeftA += len(pit[1])
+            pit[1] = []
         if marblesLeftA > 0:
             for i in range (marblesLeftA):
                 cxMarble, cyMarble = PlayerA.getRangeMarbleInRect(app)
@@ -199,65 +220,62 @@ class PlayerA:
 class PlayerB:
     def __init__(self, app):
         self.app = app
-    # def hoverPit(self): #to change color of pit 
-    #     pass
-
+    
     def moveMarblesInpit(self, app, cxTarget, cyTarget):
         app.messageTurnB = f"{app.playerBName}'s Turn"
         indexTarget = PlayerB.getIndexCircleTarget(app, cxTarget, cyTarget)
         numMarbles = len(app.marblesB[indexTarget][1])
-        nextCircle = indexTarget
-        oppCircle = 0
-        while numMarbles != 0:
-            nextCircle -= 1
-            #deposit marble in my own pit 
-            if nextCircle < 6 and nextCircle >= 0:
-                cxPit, cyPit = app.marblesB[nextCircle][0]
-                cxMarble, cyMarble = PlayerB.getRangeMarbleInCircle(app, cxPit, cyPit)
-                app.marblesB[nextCircle][1].append((cxMarble, cyMarble))
-                app.marblesB[indexTarget][1].pop(0)
-                numMarbles -= 1
-            #deposit marble in store 
-            elif nextCircle == -1:
+        if app.marblesB[indexTarget][1] != []:
+            nextCircle = indexTarget
+            oppCircle = 0
+            while numMarbles != 0:
+                nextCircle -= 1
+                #deposit marble in my own pit 
+                if nextCircle < 6 and nextCircle >= 0:
+                    cxPit, cyPit = app.marblesB[nextCircle][0]
+                    cxMarble, cyMarble = PlayerB.getRangeMarbleInCircle(app, cxPit, cyPit)
+                    app.marblesB[nextCircle][1].append((cxMarble, cyMarble))
+                    app.marblesB[indexTarget][1].pop(0)
+                    numMarbles -= 1
+                #deposit marble in store 
+                elif nextCircle == -1:
+                    cxMarble, cyMarble = PlayerB.getRangeMarbleInRect(app)
+                    app.storeBMarbles.append((cxMarble, cyMarble))
+                    app.marblesB[indexTarget][1].pop(0)
+                    numMarbles -= 1
+                #deposit marble in opponent pit
+                elif nextCircle < -1 and oppCircle >= 0 and oppCircle < 6:
+                    cxPit, cyPit = app.marblesA[oppCircle][0]
+                    cxMarble, cyMarble = PlayerB.getRangeMarbleInCircle(app,cxPit, cyPit)
+                    app.marblesA[oppCircle][1].append((cxMarble, cyMarble))
+                    app.marblesB[indexTarget][1].pop(0)
+                    numMarbles -= 1
+                    oppCircle += 1
+                #made a turn of the whole board and have to deposit in my board again
+                #case if I have a lot of marbles in chosen pit 
+                else:
+                    nextCircle = 6
+            app.playerBTurn = False
+            app.playerATurn = True 
+            #if last marble lands in store, take another turn 
+            if nextCircle == -1:
                 app.messageTurnB = f"{app.playerBName}'s Turn again"
-                cxMarble, cyMarble = PlayerB.getRangeMarbleInRect(app)
-                app.storeBMarbles.append((cxMarble, cyMarble))
-                app.marblesB[indexTarget][1].pop(0)
-                numMarbles -= 1
-            #deposit marble in opponent pit
-            elif nextCircle < -1 and oppCircle >= 0 and oppCircle < 6:
-                cxPit, cyPit = app.marblesA[oppCircle][0]
-                cxMarble, cyMarble = PlayerB.getRangeMarbleInCircle(app,cxPit, cyPit)
-                app.marblesA[oppCircle][1].append((cxMarble, cyMarble))
-                app.marblesB[indexTarget][1].pop(0)
-                numMarbles -= 1
-                oppCircle += 1
-            #made a turn of the whole board and have to deposit in my board again
-            #case if I have a lot of marbles in chosen pit 
-            else:
-                nextCircle = 5
-        app.playerBTurn = False
-        app.playerATurn = True 
-        #if last marble lands in store, take another turn 
-        if nextCircle == -1:
-            app.playerBTurn = True
-            app.playerATurn = False
+                app.playerBTurn = True
+                app.playerATurn = False
 
-        #if last marble lands in my own pit and its empty
-        elif nextCircle >= 0 and nextCircle < 6:
-            if len(app.marblesB[nextCircle][1]) == 1:
-                # capture that last marble 
-                app.marblesB[nextCircle][1] = []
-                cxMarble, cyMarble = PlayerB.getRangeMarbleInRect(app)
-                app.storeBMarbles.append((cxMarble, cyMarble))
-                # capture all the marble in opposite pit of the opponent 
-                if len(app.marblesA[nextCircle][1]) != []:
+            elif nextCircle >= 0 and nextCircle < 6:
+                if (len(app.marblesB[nextCircle][1]) == 1) and (len(app.marblesA[nextCircle][1]) >0):
+                    # capture that last marble 
+                    app.marblesB[nextCircle][1] = []
+                    cxMarble, cyMarble = PlayerB.getRangeMarbleInRect(app)
+                    app.storeBMarbles.append((cxMarble, cyMarble))
+                    # capture all the marble in opposite pit of the opponent 
                     capturedMarbles = len(app.marblesA[nextCircle][1])
                     app.marblesA[nextCircle][1] = []
                     for i in range (capturedMarbles):
                         cxMarble, cyMarble = PlayerB.getRangeMarbleInRect(app)
                         app.storeBMarbles.append((cxMarble, cyMarble))
-    
+        
     def drawMessageTurnB(self, app):
         drawLabel(app.messageTurnB, (app.boardLeft + app.boardWidth - 30), 
                   (app.boardTop - 30), fill = "black", align = "center" )
@@ -266,6 +284,7 @@ class PlayerB:
         marblesLeftB = 0
         for pit in app.marblesB:
             marblesLeftB += len(pit[1])
+            pit[1] = []
         if marblesLeftB > 0:
             for i in range (marblesLeftB):
                 cxMarble, cyMarble = PlayerB.getRangeMarbleInRect(app)
@@ -384,9 +403,11 @@ def redrawAll(app):
 
     if app.playerBTurn and (not app.playerATurn):
         playerB.drawMessageTurnB(app)
-    board.drawBoardMessage
+    if app.gameOver:
+        board.drawBoardMessage(app)
+        board.checkWinner(app)
+        #printing winner, will make it prettier after
 
-    # print(app.marblesA) 
 
 
 # def onMouseMove(app, mouseX, mouseY):
@@ -404,30 +425,64 @@ def onMousePress(app, mouseX, mouseY):
         for cx, cy in app.circlesB:
             if distance(cx, cy, mouseX, mouseY) <= app.pitRadius:
                 if app.playerBTurn and (not app.playerATurn):
-                    moveMarblesInPitB(app, cx, cy)
-    elif app.gameOver:
-        playerA.captureAllRestMarbles(app)
-        playerB.captureAllRestMarbles(app) 
-        
+                    moveMarblesInPitB(app, cx, cy)   
     
-                
+
+    # if pitsA.allEmpty(app)==True or pitsB.allEmpty(app)==True:
+    if allEmptyA(app) or allEmptyB(app):
+        app.gameOver = True
+    
+    if app.gameOver:
+        playerB.captureAllRestMarbles(app)
+        playerA.captureAllRestMarbles(app) 
+        app.playerATurn = False 
+        app.playerBTurn = False
+        return 
+        
 
 
 def moveMarblesInPitA(app, cxTarget, cyTarget):
-    app.messageTurnA = f"{app.playerAName}'s Turn"
-    playerA.moveMarblesInpit(app, cxTarget, cyTarget)
+    # app.messageTurnA = f"{app.playerAName}'s Turn"
+    indexTarget = PlayerA.getIndexCircleTarget(app, cxTarget, cyTarget)
+    if (app.marblesA[indexTarget][1]) != []:
+        playerA.moveMarblesInpit(app, cxTarget, cyTarget)
     
 
 def moveMarblesInPitB(app, cxTarget, cyTarget):
-    playerB.moveMarblesInpit(app, cxTarget, cyTarget)
+    indexTarget = PlayerB.getIndexCircleTarget(app, cxTarget, cyTarget)
+    if (app.marblesB[indexTarget][1]) != []:
+        playerB.moveMarblesInpit(app, cxTarget, cyTarget)
 
-def checkGameOver(app):
-    if pitsA.allEmpty or pitsB.allEmpty:
-        # playerA.captureAllRestMarbles(app)
-        # playerB.captureAllRestMarbles(app)  
-        app.gameOver = True
-        
+def getIndexCircleTarget(app, cxTarget, cyTarget):
+        for i in range (6):
+            if app.marblesB[i][0] == (cxTarget, cyTarget):
+                return i 
+
+def allEmptyA(app):
+        for circleCheck in app.marblesA:
+            if len(circleCheck[1]) > 0:
+                return False
+        return True 
+
+def allEmptyB(app):
+        for circleCheck in app.marblesB:
+            if len(circleCheck[1]) > 0:
+                return False
+        return True 
+
+# def checkGameOver(app):
+#     if allEmptyA(app) or allEmptyB(app):
+
+#     # if pitsA.allEmpty or pitsB.allEmpty:
+#         app.gameOver = True
     
+#         playerB.captureAllRestMarbles(app)
+        
+
+#         playerA.captureAllRestMarbles(app) 
+#         app.playerATurn = False 
+#         app.playerBTurn = False
+
 def main():
     runApp(width = 800, height = 800)
 
@@ -439,9 +494,11 @@ if __name__ == '__main__':
 
 
 ##problems 
-#if i just click and it is empty it will count it as a move 
+#problem with capture all rest marbles
+#have problem when its going on all over the board when you have lots of marbles
+#when it has to come back to its house again 
 #also does not gameOver 
-#capturing even if its empty 
+
 #gameOver 
      # if all the pits in one of the side are empty 
      #other player capture all the marbles remaining in his pits 
